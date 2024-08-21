79566c93698e1ddc1d09061cc63a71d351eff58c
Ezgi Kececi Atalay <ezgikececii@gmail.com>
2024-08-21 09:34:48 +0300

    Add files via upload

---
+++ PatikaTryCatch/PatikaTryCatch/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        bool isValidNumber = false; 
+
+        while (!isValidNumber) // Döngü, değişken true olana kadar çalışacak
+        {
+            try
+            {
+                Console.Write("Bir sayı giriniz: ");
+
+                int userNumber= Convert.ToInt32(Console.ReadLine()); // Hata yapılma durumu olan satır. Hata yoksa alt satıra, hata varsa catch bloğuna geçecek.
+
+                isValidNumber = true; // hata yoksa döngüden çıkaracak
+
+                Console.WriteLine("Girilen sayının karesi: " + Math.Pow(userNumber, 2)); // Konsola, girilen sayı değerinin karesini yazdırma
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Geçersiz veri girişi. Lütfen geçerli bir sayı değeri giriniz: "); //hata bilgisi konsola yazdırıldı
+            }
+        }
+        
+        
+
+    }
+}
